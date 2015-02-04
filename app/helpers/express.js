f6ebdb949dd0f2b8cc6613c813ea11f08180289d
Makis Tracend <makis.tracend@gmail.com>
2015-02-03 21:25:53 -0800

    + express helper

---
+++ app/helpers/express.js
@@ -0,0 +1,34 @@
+var Parent = require('brisk-security').getHelper('express'),
+	cerberus = require('cerberus'),
+	CORS = require('connect-cors'),
+	//CORS = brisk.getLib("connect-xcors"),
+	csrf = require('csurf');
+
+
+var helper = Parent.extend({
+
+	//CORS middleware
+	cors : function(req, res, next) {
+		// lookup registered apps...
+		res.header('Access-Control-Allow-Origin', "*");
+		//res.header('Access-Control-Allow-Origin', config.allowedDomains);
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type');
+		//res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+		next();
+	},
+/*
+	//CORS middleware
+	cors : function() {
+		// load configuration
+		return CORS( brisk.loadConfig('cors') );
+	},
+*/
+	cerberus: cerberus,
+
+	csrf: csrf
+
+});
+
+
+module.exports = helper;
